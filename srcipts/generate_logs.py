bd50e27b8d4bb47b7d2cbb28a8bb1a2fe53f82bd
Shweta Saini~ <Shweta.sainii786@gmail.com~>
2026-05-20 14:23:15 +0530

    Initial project structure for cybersecurity lakehouse

---
+++ srcipts/generate_logs.py
@@ -0,0 +1,36 @@
+import random
+from faker import Faker
+import pandas as pd
+
+data = []
+
+fake = Faker()
+
+events = [
+    "LOGIN_SUCCESS",
+    "FAILED_LOGIN",
+    "PASSWORD_RESET",
+    "MALWARE_ALERT",
+    "FIREWALL_BLOCK",
+    "SUSPICIOUS_IP"
+]
+
+devices = ["Windows", "Linux", "MacOS"]
+countries = ["India", "USA", "Germany", "UK", "Canada"]
+
+for i in range(10000):
+    data.append({
+        "event_time": fake.date_time_this_year(),
+        "user_id": random.randint(100, 999),
+        "ip_address": fake.ipv4(),
+        "event_type": random.choice(events),
+        "device_type": random.choice(devices),
+        "country": random.choice(countries),
+        "severity": random.choice(["LOW", "MEDIUM", "HIGH"])
+    })
+
+df = pd.DataFrame(data)
+
+df.to_csv("data/raw/security_logs.csv", index=False)
+
+print("Data genarated successfully !!!! ✅")
